a3de3a592acb48aa00ca2d622f233ee5d9e1b7f6
Moof Soup <moofsoup@gmail.com>
2024-10-06 17:22:39 -0700

    added .env boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+
+use dotenv::dotenv;
+use std::env;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let dify_api_key = env::var("DIFY_API_KEY").expect("DIFY_API_KEY not set");
+
+    match env::var("DIFY_API_KEY") {
+        Ok(api_key) => println!("Dify API Key: {}", api_key),
+        Err(e) => println!("Couldn't read DIFY_API_KEY: {}", e),
+    }
 }
+
